ea712baf92a58ce19e4422ceadee290f7a5bd764
David Jiménez <davigetto@gmail.com>
2017-05-30 16:21:48 +0200

    First commit

---
+++ HPA-netcore/LocalEntrance.cs
@@ -0,0 +1,19 @@
+﻿namespace HPASharp
+{
+    public class LocalEntrance
+    {
+        public int Id { get; set; } // id of the global abstract node
+        public int AbsNodeId { get; set; }
+        // Relative position of entrance inside cluster
+        public Position RelativePos { get; set; }
+        public int EntranceLocalIdx { get; set; } // local id
+
+        public LocalEntrance(int nodeId, int absNodeId, int localIdx, Position relativePosition)
+        {
+            Id = nodeId;
+            AbsNodeId = absNodeId;
+            EntranceLocalIdx = localIdx;
+            RelativePos = relativePosition;
+        }
+    }
+}
---
+++ HPA-netcore/Nodes.cs
@@ -0,0 +1,15 @@
+﻿namespace HPASharp
+{
+    public struct Node
+    {
+        public int Id;
+
+        public int Level;
+
+        public Node(int id, int lvl)
+        {
+            Id = id;
+            Level = lvl;
+        }
+    }
+}
